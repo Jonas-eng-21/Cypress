d48aeb4bac006d9db8052bb0819b3a621771898d
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2022-05-19 16:17:56 -0300

    Instalando o node modules e o cypress

---
+++ cypress/integration/ongs.spect.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Ongs',) () => {
+  it('devem poder realizar um cadastro', ()=>{
+
+  })
+
+  it('devem poder realizar um login no sistema', ()=>{
+
+  })
+}
