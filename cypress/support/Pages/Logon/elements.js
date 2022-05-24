a9d9a5a51f2ad69a6911edab31671d12b323191c
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2022-05-24 18:38:53 -0300

    Update: utilizando Page Objects - doc no readme -

---
+++ cypress/support/Pages/Logon/elements.js
@@ -0,0 +1,10 @@
+//elementos da página para executar as ações
+/**
+ *[data-cy=id]
+ *[data-cy=button-login]
+ */
+
+export const ELEMENTS = {
+  id: '[data-cy=id]',
+  submit: '[data-cy=button-login]'
+}
